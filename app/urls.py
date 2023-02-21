1e1414ab26d222290e8745c35d52ea67d3b7383c
Jules257 <julesnzejimana@gmail.com>
2023-02-21 19:34:29 +0100

    urls categorie

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 
-from app.views import home
+from app.views import home,categories
 
 
 
 
 urlpatterns = [
     path('',home.index, name='home'),
+    
+    path('categories/',categories.index, name='categories_index'),
+    path('categories/store',categories.store, name='categories_store'),
+    path('categories/delete/<int:id>',categories.delete, name='categories_delete'),
+    path('categories/update',categories.update, name='categories_update'),
+    path('categories/edit/<int:id>',categories.edit, name='categories_edit'),
     
    
     
