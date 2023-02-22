40de9da4f33ee16e6137164c695937d58a82afbf
Jules257 <julesnzejimana@gmail.com>
2023-02-22 20:56:43 +0100

    some changes

--- cooperative/settings.py
+++ cooperative/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'crispy_forms'
+    'crispy_forms',
+    'django_filters'
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
