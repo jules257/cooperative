a8abc84625d101c709c314cf9f10a5f6b54c0390
Jules257 <julesnzejimana@gmail.com>
2023-02-22 20:54:12 +0100

    Add something in init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,3 +5,17 @@ from .comembre import Comembre
 from .credit import Credit
 from .cotisation import Cotisation
 from .remboursement import Remboursement
+
+from .membre import Membre
+from .categorie import Categorie
+from .cooperative import Cooperative
+from .comembre import Comembre
+from .credit import Credit
+from .cotisation import Cotisation
+from .remboursement import Remboursement
+
+ 
+
+
+
+
